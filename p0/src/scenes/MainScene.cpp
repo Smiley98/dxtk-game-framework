93b2afb66fa56e67052b5b41303d75a667358939
Smiley98 <smileysofficialcinema@gmail.com>
2022-07-28 18:29:10 -0400

    Added td building and changed view & perspective

--- p0/src/scenes/MainScene.cpp
+++ p0/src/scenes/MainScene.cpp
@@ -17,7 +17,7 @@ namespace scene
 
 		m_shader->SetLightingEnabled(true);
 		m_shader->SetPerPixelLighting(true);
-		m_shader->SetLightDirection(0, Vector3::UnitY);
+		m_shader->SetLightDirection(0, Vector3::UnitZ * -1.0f);
 
 		m_shader->SetAmbientLightColor(Colors::Red);
 		m_shader->SetDiffuseColor(Colors::Green);
@@ -27,7 +27,8 @@ namespace scene
 		//m_shader->SetTextureEnabled(true);
 		//m_shader->SetTexture(m_texture.Get());
 		//DX::ThrowIfFailed(CreateDDSTextureFromFile(device, L"assets/textures/van.dds", nullptr, m_texture.ReleaseAndGetAddressOf()));
-		m_vbo = Model::CreateFromVBO(device, L"assets/meshes/cup.vbo", m_shader);
+		//m_vbo = Model::CreateFromVBO(device, L"assets/meshes/td.vbo", m_shader);	// Downtown Toronto was exported correctly ;)
+		m_vbo = Model::CreateFromVBO(device, L"assets/meshes/td.vbo", m_shader);
 	}
 
 	MainScene::~MainScene()
@@ -42,9 +43,9 @@ namespace scene
 	{
 		const RECT size = graphics->GetOutputSize();
 		const float aspectRatio = float(size.right) / float(size.bottom);
-		float fovAngleY = 35.0f * XM_PI / 180.0f;
+		float fovAngleY = 60.0f * XM_PI / 180.0f;
 		fovAngleY = aspectRatio < 1.0f ? fovAngleY * 2.0f : fovAngleY;
-		m_projection = Matrix::CreatePerspectiveFieldOfView(fovAngleY, aspectRatio, 0.01f, 100.0f);
+		m_projection = Matrix::CreatePerspectiveFieldOfView(fovAngleY, aspectRatio, 0.01f, 10000.0f);
 	}
 
 	void MainScene::OnBegin()
@@ -65,18 +66,17 @@ namespace scene
 
 	void MainScene::OnUpdate(const DX::StepTimer& timer, const DirectX::GamePad& gamePad, const DirectX::Keyboard& keyboard, const DirectX::Mouse& mouse)
 	{
-		// eye { 0.0f, 0.0f, -10.0f } up UnitY for front view, eye { 0.0f, -10.0f, 0.0f } up UnitZ for top view
-		const Vector3 eye(0.0f, -10.0f, 0.0f);
+		const Vector3 eye(0.0f, -100.0f, 1000.0f);
 		const Vector3 at(0.0f, 0.0f, 0.0f);
-		m_view = Matrix::CreateLookAt(eye, at, Vector3::UnitZ);
-		m_world = Matrix::CreateRotationY(float(timer.GetTotalSeconds() * XM_PIDIV4));
+		m_view = Matrix::CreateLookAt(eye, at, Vector3::UnitY);
+		m_world = Matrix::CreateRotationZ(float(timer.GetTotalSeconds() * XM_PIDIV4));
 	}
 
 	void MainScene::OnRender(std::shared_ptr<DX::DeviceResources> graphics)
 	{
 		auto context = graphics->GetD3DDeviceContext();
 
-		XMMATRIX local = m_world * Matrix::CreateTranslation(1.0f, 0.0f, 1.0f);
+		XMMATRIX local = Matrix::CreateScale(1.0f) *  m_world * Matrix::CreateTranslation(1.0f, 0.0f, 1.0f);
 		m_vbo->Draw(context, *m_states, local, m_view, m_projection);
 	}
 }
