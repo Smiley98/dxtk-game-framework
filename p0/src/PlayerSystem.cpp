f86b0a11b017ac969629098143aee9a550fce0a4
Smiley98 <smileysofficialcinema@gmail.com>
2022-12-18 20:25:56 -0500

    Unified gamepad and keyboard controls

--- p0/src/PlayerSystem.cpp
+++ p0/src/PlayerSystem.cpp
@@ -22,43 +22,80 @@ namespace Players
 			constexpr float VAN_MOVE_VEL = 500.0f;
 			constexpr float VAN_MOVE_ACC = 500.0f;
 
+			enum MovementState {
+				FORWARD,
+				BACKWARD,
+				NONE
+			} translation = NONE, rotation = NONE;
+
 #if GAMEPAD
-			GamePad::State state = input.gamePad.GetState(0);
+			GamePad::State pad = GamePad::Get().GetState(i);
 
-			if (state.IsLeftThumbStickLeft())
-				transform.DeltaRotateZ(av);
-			if (state.IsLeftThumbStickRight())
-				transform.DeltaRotateZ(-av);
+			if (pad.IsAPressed())
+				translation = FORWARD;
+			else if (pad.IsXPressed())
+				translation = BACKWARD;
+			else
+				translation = NONE;
 
-			if (state.IsAPressed())
-				transform.DeltaTranslate(transform.Forward() * av);
-			if (state.IsXPressed())
-				transform.DeltaTranslate(transform.Forward() * -av);
+			if (pad.IsLeftThumbStickLeft())
+				rotation = FORWARD;
+			else if (pad.IsLeftThumbStickRight())
+				rotation = BACKWARD;
+			else
+				rotation = NONE;
 #endif
 			
 #if KEYBOARD
-			Keyboard::State state = input.keyboard.GetState();
-			if (state.W)
-				body.hAcc = VAN_MOVE_ACC;
-			else if (state.S)
-				body.hAcc = -VAN_MOVE_ACC;
+			Keyboard::State keys = input.keyboard.GetState();
+			if (keys.W)
+				translation = FORWARD;
+			else if (keys.S)
+				translation = BACKWARD;
 			else
+				translation = NONE;
+
+			if (keys.A)
+				rotation = FORWARD;
+			if (keys.D)
+				rotation = BACKWARD;
+#endif
+
+			switch (translation)
 			{
+			case FORWARD:
+				body.hAcc = VAN_MOVE_ACC;
+				break;
+
+			case BACKWARD:
+				body.hAcc = -VAN_MOVE_ACC;
+				break;
+
+			case NONE:
 				body.hAcc = 0.0f;
 				body.hVel *= 0.95f;
+				break;
 			}
 
-			// Increase acceleration (via frication) if breaking (opposite to current velocity)
+			switch (rotation)
+			{
+			case FORWARD:
+				transform.DeltaRotateZ(player.steering * dt);
+				break;
+
+			case BACKWARD:
+				transform.DeltaRotateZ(-player.steering * dt);
+				break;
+
+			case NONE:
+				break;
+			}
+
+			// Increase deceleration (via frication) if breaking (opposite to current velocity)
 			if (body.hVel > 0.0f && body.hAcc < 0.0f || body.hVel < 0.0f && body.hAcc > 0.0f)
 			{
 				body.hVel *= 0.9f;
 			}
-
-			if (state.A)
-				transform.DeltaRotateZ(player.steering * dt);
-			if (state.D)
-				transform.DeltaRotateZ(-player.steering * dt);
-#endif
 			body.hVel = std::clamp(body.hVel, -VAN_MOVE_VEL, VAN_MOVE_VEL);
 		}
 	}
